0d432b8aefbb2c06bf42ad676f37f99f3f271c0d
Selasie5 <selasisepenu5@gmail.com>
2025-01-14 12:27:34 +0000

    Build hero section

---
+++ app/components/ui/Navbar.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface Links {
+    name: string;
+    path: string;
+}
+const Navbar = () => {
+    const Links: Links[]=[
+        {
+            name:"Home",
+            path:"/"
+        },
+        {
+            name:"About",
+            path:"/"
+        },
+        {
+            name:"Events",
+            path:"/"
+        },
+        {
+            name:"Gallery",
+            path:"/"
+        },
+        {
+            name:"Contact",
+            path:"/"
+        },
+    ]
+  return (
+    <section className='hero-navbar w-full px-10 py-6'>
+        <nav className='flex justify-between items-center'>
+          <div>
+            <Image src="/mc_logo.png" alt="musicians-corner-logo" width={100} height={100}/>
+            </div> 
+
+            <div className="flex justify-between items-center space-x-10">
+                {Links.map((link, index)=>
+                (
+                    <Link href={link.path} key={index} className='text-white hover:text-lime-500'>
+                        {link.name}
+                        </Link>
+                ))}
+                </div> 
+                <div>
+                    <button className='bg-lime-500 text-black text-sm px-5 py-5 uppercase flex justify-center items-center gap-4'>
+                        <hr className="bg-black h-[0.1rem] border-0 w-10"/> {" "}
+                        Partner With Us
+                        </button>
+                </div>
+        </nav>
+    </section>
+  )
+}
+
+export default Navbar
