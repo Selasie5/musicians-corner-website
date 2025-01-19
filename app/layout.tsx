7095b9410ac1485ff3c89da5245759fcf65796ea
Selasie5 <selasisepenu5@gmail.com>
2025-01-19 12:26:12 +0000

    Implement analytical feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {Analytics} from "@vercel/analytics"
 
 
 //Component & Library Imports
@@ -27,6 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics/>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
